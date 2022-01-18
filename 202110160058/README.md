99f2d508dce1f5d235476624e73e47141b32378a
Eliot Khachi <eliotkh12@gmail.com>
2022-01-17 20:15:10 -0800

    Wiped

--- 202110160058/README.md
+++
@@ -1,8 +0,0 @@
-# Angle of Attack 
-
-The angle between the velocity of the fluid and the chordline of the airfoil.
-
-[202110160051](../202110160051) - Airfoil 
-
-## Tags
-#aerodynamics
--- recommit.sh
+++ recommit.sh
@@ -1,10 +1,14 @@
 #!bin/bash
+ZETDIR_PUBLIC="$ZETDIR_PUBLIC"_old
 for dir in $(ls "$ZETDIR_PUBLIC" | grep -v '^.*.md$')
 do
 	readme="$dir/README.md"
 	zet="$ZETDIR_PUBLIC/$readme"
 	folder="$ZETDIR_PUBLIC/$dir"
 	msg=$(cat $zet | head -n 1)
-	cp $folder ./
-
+	cp -r $folder ./
+	git add $readme
+	git commit -m "${msg:2}"
 done
+git push
+
