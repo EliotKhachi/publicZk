3782734c4fe98b9ea0907f43eaa9a04bd16bd81e
Eliot Khachi <eliotkh12@gmail.com>
2022-01-19 11:20:45 -0800

    Jim Kwik on Habits (Updated: Wed Jan 19 19:20:42 UTC 2022)

--- recommit.sh
+++ recommit.sh
@@ -1,10 +1,10 @@
 #!bin/bash
-ZETDIR_PUBLIC="$ZETDIR_PUBLIC"_old
-for dir in $(ls "$ZETDIR_PUBLIC" | grep -v '^.*.md$')
+ZETDIR="$ZETDIR"_old
+for dir in $(ls "$ZETDIR" | grep -v '^.*.md$')
 do
 	readme="$dir/README.md"
-	zet="$ZETDIR_PUBLIC/$readme"
-	folder="$ZETDIR_PUBLIC/$dir"
+	zet="$ZETDIR/$readme"
+	folder="$ZETDIR/$dir"
 	msg=$(cat $zet | head -n 1)
 	cp -r $folder ./
 	git add $readme
