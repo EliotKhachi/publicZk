9d717001cd5f05b261485d043cac8679d73eca22
eliot <eliotkh12@gmail.com>
2023-09-29 22:49:26 -0700

    AWS Storage Gateway (Updated: Sat Sep 30 05:49:26 AM UTC 2023)

--- change_zet_links.sh
+++ change_zet_links.sh
@@ -2,6 +2,6 @@
 for dir in $(ls | grep -vE "\.sh$|\.md$|\.json$"); do
     echo $dir
     cd $dir
-    sed -n 's#\[\(.*\)\](.*tree/main/\(.*\))#\[\1\](./\2)#g#p' README.md # changes hyperlinks to local zettel links
+    sed -n 's#\[\(.*\)\](.*tree/main/\(.*\))#\[\1\](./\2)#p' README.md # changes hyperlinks to local zettel links
     cd ..
 done
