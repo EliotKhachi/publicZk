f1de537683198c1eacfe1e4c9c36d64ecc73ebca
eliot <eliotkh12@gmail.com>
2023-09-29 21:54:45 -0700

    AqualinkD Project (Updated: Sat Sep 30 04:54:45 AM UTC 2023)

--- change-image-links.sh
+++ change-image-links.sh
@@ -2,6 +2,11 @@
 for dir in $(ls | grep -vE "\.sh$|\.md$|\.json$"); do
     echo $dir
     cd $dir
-    ls
+    # images=$(sed -n '/!\[image\]/p' README.md)
+    sed -n 's/^\[!image\].*zettelimages\/\(.*\.png\))/\1/p' README.md
+    # for image in $images; do
+    #     # echo $image
+    #     aws s3 cp s3://zettelimages/$(echo "$image" | sed -n 's/.*zettelimages\/\(.*\.png\))/\1/p') .
+    # done
     cd ..
 done
