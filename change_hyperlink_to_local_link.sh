c8088533e22d614070cd75e570337135c439bf04
eliot <eliotkh12@gmail.com>
2023-09-29 23:12:56 -0700

    Amazon (QLDB) - Quantum Ledger Database (Updated: Sat Sep 30 06:12:56 AM UTC 2023)

--- change_hyperlink_to_local_link.sh
+++ change_hyperlink_to_local_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -n 's/' README.md # changes hyperlinks to local zettel links
+sed -n 's/\(.\/202.*)\)/.\1/p' 202309152346/README.md # changes hyperlinks to local zettel links
 # for dir in $(ls | grep -vE "\.sh$|\.md$|\.json$"); do
 #     echo $dir
 #     cd $dir
