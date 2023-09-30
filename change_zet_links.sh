10557a39b832f6e211dadf3ff0a0fba30a7dc671
eliot <eliotkh12@gmail.com>
2023-09-29 22:45:29 -0700

    Availability Zone (Updated: Sat Sep 30 05:45:29 AM UTC 2023)

--- change_zet_links.sh
+++ change_zet_links.sh
@@ -2,10 +2,6 @@
 for dir in $(ls | grep -vE "\.sh$|\.md$|\.json$"); do
     echo $dir
     cd $dir
-    sed -n 's#\[.*\](.*tree/main/\(.*\))#(.\1#p' README.md # replace s3 bucket image link with local image link
-    # Convert
-    # [Software Architecture Layers](https://github.com/EliotKhachi//publicZk/tree/ma    in/202209172045)
-    # to
-    # [Software Architecture Layers](./202209172045)
+    sed -n 's#\[\(.*\)\](.*tree/main/\(.*\))#\[\1\](./\2)#p' README.md # changes hyperlinks to local zettel links
     cd ..
 done
