9b3d404443959f20eaef6b37bfc34acc665c60be
eliot <eliotkh12@gmail.com>
2023-09-29 23:08:23 -0700

    Deep Work by Cal Newport (Updated: Sat Sep 30 06:08:23 AM UTC 2023)

--- .archive/change_zet_links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for dir in $(ls | grep -vE "\.sh$|\.md$|\.json$"); do
-    echo $dir
-    cd $dir
-    sed -i 's#\[\(.*\)\](.*tree/main/\(.*\))#\[\1\](./\2)#g' README.md # changes hyperlinks to local zettel links
-    cd ..
-done
