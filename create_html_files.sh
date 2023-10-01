d965dba71b9807107633439d46b220a4de0460f6
eliot <eliotkh12@gmail.com>
2023-10-01 15:21:03 -0700

    AqualinkD Project (Updated: Sun Oct 1 10:21:02 PM UTC 2023)

--- create_html_files.sh
+++ create_html_files.sh
@@ -19,8 +19,8 @@ for dir in $(ls | grep -Ev '.md$|.json$|.css$|.html$|.sh$'); do
     
     title=$(head -n 1 "$input_file" | sed -n 's/^# //p')
     
-    # declare filepath to store HTML file temporarily 
-    output_file="/tmp/zettelkasten/$(basename $(pwd)).html"
+    # Store HTML file in current directory 
+    output_file="zettel.html"
 
     # Convert Markdown to HTML using pandoc
     pandoc -f gfm "$input_file" -o "$output_file" --template="$ZETDIR/custom_template.html" --metadata title="$title"
@@ -34,9 +34,7 @@ for dir in $(ls | grep -Ev '.md$|.json$|.css$|.html$|.sh$'); do
 
     # Fix zettel links within html per website's zettel storage structure:
     # zettelkasten/zettel-id.html
-    sed -i 's#<a href="\.\./\([0-9]\{12\}\)">#<a href="\./\1">#g' $output_file
+    sed -i 's#<a href="\.\./\([0-9]\{12\}\)">#<a href="\../\1/index.html">#g' $output_file
 
-    # upload to website s3 zettelkasten
-    aws s3 cp $output_file s3://public-zettelkasten
     cd ..
 done
