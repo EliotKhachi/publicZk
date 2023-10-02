e5655a554dc59167129c80c4e64bf956fa7439fc
eliot <eliotkh12@gmail.com>
2023-10-01 19:55:22 -0700

    added zettel json script

--- .archive/create_zettels_json.sh
+++ .archive/create_zettels_json.sh
@@ -10,14 +10,13 @@ for dir in $(ls); do
     id=$dir
 
 
-    ## Extract tags and store them in array
+    ## Extract tags and store them in array ##
     # In this order: read the last line of the README.md file, remove " Tags" (in case there are no tags in this zettel), remove #s, capture each group of characters other than spaces ( \([^ ]\+\) ) and encase them with quotes, and replace spaces with commas.
-    # Extract tags from the last line
     tags=$(tail -n 1 "$dir/README.md" |sed 's/ Tags//g' | sed 's/#//g' | sed 's/\([^ ]\+\)/"\1"/g' | sed 's/ /,/g')
 
     # Add zettel data to the array
     zettel_data="{\"id\": $id, \"tags\": [${tags[*]}]}"
-    zettels+=("$zettel_data")
+    zettels+=("$zettel_data") # replace every ,] (invalid json) with ] (
   fi
 done
 
@@ -27,8 +26,7 @@ IFS=, zettels_json="${zettels[*]}"
 # Create the JSON output
 json_output="{\"zettels\": [$zettels_json]}"
 
-# Write the JSON output to a file
-echo "$json_output" > zettels.json
+# Write the JSON output to a file (while removing anything that makes jsons invalid, i.e. ,] and ,}
+echo "$json_output" | sed 's#,\]#\]#g' | sed 's#,}#}#g' > zettels.json
 
 echo "JSON data saved to zettels.json"
-
